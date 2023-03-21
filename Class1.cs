94c75618ae35772d86b5ee176d273eb3cb451ca6
cinvenz <vincenzobortune@gmail.com>
2023-03-21 18:07:46 +0100

    insert search metod

--- Class1.cs
+++ Class1.cs
@@ -6,63 +6,95 @@ using System.Threading.Tasks;
 
 public class Biblioteca
 {
-    bool registrato;
+    public bool registrato;
 
-    List<Documento> documenti = new List<Documento>();
-    List<Utente> utenti = new List<Utente>();
-    List<Prestito> prestiti = new List<Prestito>();
+    public List<Documento> documenti = new List<Documento>();
+    public List<Utente> utenti = new List<Utente>();
+    public List<Prestito> prestiti = new List<Prestito>();
 
     public Biblioteca(bool registrato)
     {
         this.registrato = registrato;
     }
 
-   
+    public List<Documento> RicercaDocumento(string parolaCercata)
+    {
+        List<Documento> risultati = new List<Documento>();
+
+        foreach (Documento documento in documenti)
+        {
+            if (documento.Codice.ToString().Contains(parolaCercata) || documento.Titolo.Contains(parolaCercata))
+            {
+                risultati.Add(documento);
+            }
+        }
+
+        return risultati;
+    }
 
 }
 public class Utente
 {
-    string Cognome { get; set; } = "";
-    string Nome { get; set; } = "";
-    string Email { get; set; } = "";
-    string Password { get; set; } = "";
-    string RecapitoTelefonico { get; set; } = "";
+    public string Cognome { get; set; } = "";
+    public string Nome { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Telefono { get; set; } = "";
 }
 
 public class Documento
 {
-    public string Codice { get; set; } = "";
-    string Titolo { get; set; } = "";
-    int Anno { get; set; }
-    string Settore { get; set; } = "";
-    int Scaffale { get; set; }
-    Autore? Autore { get; set; }
-    
+    public int Codice { get; set; }
+    public string Titolo { get; set; } = "";
+    public int Anno { get; set; }
+    public string Settore { get; set; } = "";
+    public int Scaffale { get; set; }
+    public Autore? Autore { get; set; }
+
+    public Documento(string codice, string titolo, int anno, string settore, int scaffale, Autore? autore)
+    {
+        Codice = int.Parse(codice);
+        Titolo = titolo;
+        Anno = anno;
+        Settore = settore;
+        Scaffale = scaffale;
+        Autore = autore;
+    }
+
 }
 
 
 public class Libro : Documento
 {
-    int NumeroPagine { get; set; }
+    public Libro(string codice, string titolo, int anno, string settore, int scaffale, Autore? autore) : base(codice, titolo, anno, settore, scaffale, autore)
+    {
+    }
+
+    public int NumeroPagine { get; set; }
 }
 
 public class DVD : Documento
 {
-    int Durata { get; set; }
+    public DVD(string codice, string titolo, int anno, string settore, int scaffale, Autore? autore) : base(codice, titolo, anno, settore, scaffale, autore)
+    {
+    }
+
+    public int Durata { get; set; }
 }
 
 
 public class Autore
 {
-    string Nome { get; set; } = "";
-    string Cognome { get; set; } = "";
+    public string Nome { get; set; } = "";
+    public string Cognome { get; set; } = "";
 }
 
 public class Prestito
 {
-    Documento? Documento { get; set; }
-    Utente? Utente { get; set; }
-    DateTime Dal { get; set; }
-    DateTime Al { get; set; }
+    public Documento? Documento { get; set; }
+    public Utente? Utente { get; set; }
+    public DateTime Dal { get; set; }
+    public DateTime Al { get; set; }
 }
+
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿List<Documento> documenti = new List<Documento>();
-List<Utente> utenti = new List<Utente>();
-List<Prestito> prestiti = new List<Prestito>();
+﻿Biblioteca biblioteca = new Biblioteca(registrato: true);
+
+// Aggiungere alcuni documenti alla lista della biblioteca
+biblioteca.documenti.Add(new Libro { Codice = "001", Titolo = "Introduzione alla programmazione", Anno = 2021, Settore = "Informatica", Scaffale = 1, NumeroPagine = 200 });
+biblioteca.documenti.Add(new DVD { Codice = "002", Titolo = "Programmazione avanzata", Anno = 2022, Settore = "Informatica", Scaffale = 2, Durata = 120 });
+biblioteca.documenti.Add(new Libro { Codice = "003", Titolo = "Programmazione orientata agli oggetti", Anno = 2021, Settore = "Informatica", Scaffale = 3, NumeroPagine = 250 });
+
+// Ricerca documenti contenenti la parola "programmazione"
+List<Documento> risultati = biblioteca.RicercaDocumento("programmazione");
+
+// Stampare i risultati
+Console.WriteLine($"Trovati {risultati.Count} documenti:");
+foreach (Documento documento in risultati)
+{
+    Console.WriteLine($"{documento.Codice}: {documento.Titolo}");
+}
